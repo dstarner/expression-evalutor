8e1ab9ceefb64511fdde229080c0a050a6faccd7
Dan Starner <starner.daniel5@gmail.com>
2017-02-10 14:29:52 -0500

    Basic tokenizer

--- main.cpp
+++ main.cpp
@@ -2,7 +2,48 @@
 
 #include "token.h"
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char *argv[]) {
+
+    // Ensure that there is another argument
+    if (argc != 2) {
+        fprintf(stderr, "There must be %d arguments!", 2);
+        return 1;
+    }
+
+    FILE *input_file;
+
+    input_file = fopen(argv[1], "r");
+
+    if (!input_file) {
+        printf("File not found.");
+        return -1;
+    }
+
+    char expression[1024];
+
+    if (feof (input_file)) {
+        printf("Invalid file.");
+        return -1;
+    }
+
+    fgets(expression, sizeof (expression), input_file);
+
+    // Token size needed
+    int token_arr_size = 0;
+    token_arr_size = determine_token_size(expression);
+
+    if (token_arr_size < 0) {
+        printf("Invalid token.");
+        return -1;
+    }
+
+    token tokens[token_arr_size];
+
+    build_tokens(tokens, token_arr_size, expression);
+
+    for (int i=0; i < token_arr_size; i++) {
+        printf("op: %c, val: %d\n", tokens[i].op_value, tokens[i].int_value);
+    }
+
     return 0;
 }
--- token.h
+++ token.h
@@ -2,10 +2,12 @@
 // Created by Dan Starner on 2/8/17.
 //
 
+#include <ctype.h>
+
 #ifndef INC_306_CALCULATOR_TOKEN_H
 #define INC_306_CALCULATOR_TOKEN_H
 
-struct token {
+typedef struct {
 
     // Values of token
     char op_value;
@@ -14,6 +16,139 @@ struct token {
     // Type of token
     int is_operand;
     int is_operator;
-};
+} token;
+
+int get_length(token* tokens) {
+    return sizeof(tokens) / sizeof(token);
+}
+
+
+
+bool is_rn(char c) {
+
+    return c == 'I' || c == 'V' || c == 'X' || c == 'M' || c == 'D' || c == 'L';
+
+}
+
+
+
+/**
+ * This function determines the number of tokens that will be needed for a string
+ * to correctly be parsed.
+ * @param string
+ * @return int
+ */
+int determine_token_size(char* string) {
+    int needed = 0;
+
+    int i = 0;
+    while(string[i]) {
+        string[i] = toupper(string[i]);
+        i++;
+    }
+
+    i = 0;
+    // Iterate over every string
+    while (string[i] != '\0') {
+        // Check what string[i] is.
+
+        // Space, skip
+        if (string[i] == 32 || string[i] == 10) {
+            i++;
+        }
+
+        // If number
+        else if (48 <= string[i] && string[i] <= 57) {
+            int num = 0;
+            // Keep incrementing i until not number
+            while (48 <= string[i] && string[i] <= 57) {
+                num *= 10;
+                num += (string[i] - '0');
+                i++;
+            }
+            needed++;
+        }
+
+        // Operator
+        else if (string[i] == 42 || string[i] == 43 || string[i] == 45 || string[i] == 47) {
+            needed++;
+            i++;
+        }
+
+        // If roman numeral (i/I, x/X, v/V)
+        else if (is_rn(string[i])) {
+
+            // Keep incrementing until not roman numeral
+            while (is_rn(string[i])) {
+                i++;
+            }
+            needed++;
+        }
+
+        else {
+            return -1;
+        }
+    }
+
+    return needed;
+}
+
+
+void build_tokens(token* tokens, int size, char* string) {
+
+    int cur_token = 0;
+
+    int i = 0;
+    while(string[i]) {
+        string[i] = toupper(string[i]);
+        i++;
+    }
+
+    i = 0;
+    // Iterate over every string
+    while (string[i] != '\0') {
+        // Check what string[i] is.
+
+        // Space, skip
+        if (string[i] == 32 || string[i] == 10) {
+            i++;
+        }
+
+            // If number
+        else if (48 <= string[i] && string[i] <= 57) {
+            int num = 0;
+            // Keep incrementing i until not number
+            while (48 <= string[i] && string[i] <= 57) {
+                num *= 10;
+                num += (string[i] - '0');
+                i++;
+            }
+            token tok = {.int_value=num, .op_value=0, .is_operand=1, .is_operator=0};
+            tokens[cur_token] = tok;
+            cur_token++;
+        }
+
+            // Operator
+        else if (string[i] == 42 || string[i] == 43 || string[i] == 45 || string[i] == 47) {
+            token tok = {.int_value=0, .op_value=string[i], .is_operand=0, .is_operator=1};
+            tokens[cur_token] = tok;
+            cur_token++;
+            i++;
+        }
+
+            // If roman numeral (i/I, x/X, v/V)
+        else if (is_rn(string[i])) {
+
+            // Keep incrementing until not roman numeral
+            while (is_rn(string[i])) {
+                i++;
+            }
+        }
+    }
+
+
+}
+
 
 #endif //INC_306_CALCULATOR_TOKEN_H
+
